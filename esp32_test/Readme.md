2cf9bc4339f57b91b766b0885a6bc59daebf2de1
kelson8 <kelson@kelsoncraft.net>
2026-07-10 20:13:03 -0400

    Make esp32_test turn red led on for power
    * Add readme to esp32_test, and update cargo.toml with some new packages to use later.

---
+++ esp32_test/Readme.md
@@ -0,0 +1,9 @@
+# ESP32 Test
+
+### Running/Building
+To build this project, run `cargo build`.
+
+To run this project on an ESP32:
+1. Make sure the code builds.
+2. Plug up the ESP32 while holding the `BOOT` button via Micro Usb to your PC.
+3. Use the command `cargo run` to flash the project to the ESP32.
--- esp32_test/src/main.rs
+++ esp32_test/src/main.rs
@@ -11,8 +11,29 @@ use std::{
     time::Duration
 };
 
+// I2C
+// use esp_hal::i2c::master::Config as I2cConfig; // for convenience, importing as alias
+// use esp_hal::i2c::master::I2c;
+// use esp_hal::time::Rate;
+//
+// // OLED
+// use ssd1306::{I2CDisplayInterface, Ssd1306Async, prelude::*};
+//
+// // Embedded Graphics
+// use embedded_graphics::{
+//     mono_font::{MonoTextStyleBuilder, ascii::FONT_6X10},
+//     pixelcolor::BinaryColor,
+//     prelude::Point,
+//     prelude::*,
+//     text::{Baseline, Text},
+// };
+
+
 // I got this working!
 // I had to hold the BOOT button on the ESP32 while I was also running 'cargo run'
+
+// Well, I tried to add the ssd1306, embedded-graphics, esp-hal, and tokio creates.
+// But this just broke the build I guess.
 
 // Hmm, I'm not sure how to make this into another function...
 // fn toggle_leds() -> Result<()> {
@@ -46,19 +67,46 @@ fn main() -> Result<()> {
     // New
     let peripherals = Peripherals::take().unwrap();
 
+    // Init I2C
+    // let i2c_bus = I2c::new(
+    //     peripherals.i2c0,
+    //     // I2cConfig is alias of esp_hal::i2c::master::I2c::Config
+    //     I2cConfig::default().with_frequency(Rate::from_khz(400)),
+    // )
+    //     .unwrap()
+    //     .with_scl(peripherals.pins.gpio22)?
+    //     .with_sda(peripherals.pins.gpio21)?
+    //     .into_async();
+
+    // Init SSD1306
+    // let interface = I2CDisplayInterface::new(i2c_bus);
+    // // initialize the display
+    // let mut display = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+    // .into_buffered_graphics_mode();
+    // display.init().await.unwrap();
+
     // toggle_leds()?;
 
-    let mut blue_led = PinDriver::output(peripherals.pins.gpio26)?;
+    // Power LED
+    let mut red_led = PinDriver::output(peripherals.pins.gpio26)?;
 
-    loop {
-        // blue_led.set_high().expect("Panic message");
-        blue_led.set_high()?;
-        sleep(Duration::from_millis(500));
+    // Always turn on the power LED by default
+    red_led.set_high()?;
 
-        // blue_led?.set_low().expect("Panic message");
-        blue_led.set_low()?;
-        sleep(Duration::from_millis(500));
-    }
+    // Misc LEDs
+    // let mut blue_led = PinDriver::output(peripherals.pins.gpio25)?;
+    // let mut green_led = PinDriver::output(peripherals.pins.gpio27)?;
+
+    // Blink the other LEDs
+    // loop {
+    //     blue_led.set_high()?;
+    //     green_led.set_high()?;
+    //     sleep(Duration::from_millis(500));
+    //
+    //     blue_led.set_low()?;
+    //     green_led.set_low()?;
+    //     sleep(Duration::from_millis(500));
+    // }
 
     // log::info!("Hello, world!");
 
